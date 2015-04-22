ec9b1ff611af28452d5814ea8cc6a011b6252e0e
Richard Fussenegger <richard@fussenegger.info>
2015-04-22 19:22:00 +0200

    Changed license from UNLICENSE to AGPLv3.

--- bin/template/server-subdomain.sh
+++ bin/template/server-subdomain.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+#! ---------------------------------------------------------------------------------------------------------------------
+# This file is part of fleshgrinder/nginx-configuration.
+#
+# fleshgrinder/nginx-configuration is free software: you can redistribute it and/or modify it under the terms of the GNU
+# Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# fleshgrinder/nginx-configuration is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along with fleshgrinder/nginx-configuration.
+# If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Template for server configuration.
+#
+# @author Richard Fussenegger <richard@fussenegger.info>
+# @copyright 2015 (c) Richard Fussenegger
+# @license https://www.gnu.org/licenses/agpl-3.0.html AGPLv3
+# ----------------------------------------------------------------------------------------------------------------------
+
 cat > "${CONF_PATH}" << EOT
 # ${CONF_PATH}
 
